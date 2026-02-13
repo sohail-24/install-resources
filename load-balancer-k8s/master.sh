95314ab82c00e4a0bc6447475f9f5ec73379b253
MD. Sohail <mdsohail88008@gmail.com>
2026-02-13 11:53:50 +0530

    Update master.sh

--- load-balancer-k8s/master.sh
+++ load-balancer-k8s/master.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
-echo "=== Initializing Kubernetes Control Plane ==="
+echo "========== INITIALIZING CONTROL PLANE =========="
 
 kubeadm init \
   --pod-network-cidr=192.168.0.0/16 \
   --upload-certs
 
-echo "=== Setting up kubectl for root ==="
+echo "Setting up kubectl..."
 
 mkdir -p $HOME/.kube
 cp /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-unset KUBECONFIG
-
-kubectl get nodes
+echo "Installing Calico CNI..."
 
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/calico.yaml
 
+echo "Waiting for nodes..."
+sleep 20
+kubectl get nodes
 
-echo "=== Control Plane initialized ==="
-echo "Now apply Calico usind this command manually."
+echo "========== CONTROL PLANE READY =========="
+echo "Now generate worker join command:"
+echo "kubeadm token create --print-join-command"
